e1e6dab7d30b96fdc072f2f922c9fb2e78faea7a
harshnatuskar <harshnatuskar10@gmail.com>
2023-11-09 14:31:52 +0530

    image gen demp app

---
+++ next.config.js
@@ -0,0 +1,15 @@
+const nextConfig = {
+    reactStrictMode: true,
+    images: {
+      remotePatterns: [
+        {
+          protocol: "https",
+          hostname: "replicate.com",
+        },
+        {
+          protocol: "https",
+          hostname: "replicate.delivery",
+        },
+      ],
+    },
+  };
